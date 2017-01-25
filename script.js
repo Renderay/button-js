b3d9a3735754cc8e7b2395ad75bcd8c2ab9498db
Renderay <polupan.andrew@gmail.com>
2017-01-25 20:40:22 +0300

    Предварительно готово

---
+++ script.js
@@ -0,0 +1,17 @@
+var button = document.getElementById("button");
+var i = 1;
+var day = new Date();
+
+var gDay = day.getDay();
+var gMonth = day.getMonth();
+var gHours = day.getHours();
+
+
+button.onclick = function () {
+if (i == 1) {
+alert("Вы нажали на кнопку\r\n" + day);
+i++;
+} else {
+  alert("Вы уже нажали на кнопку\r\n" + day);
+}
+}
